4284c75314732eb031d956cc819ce9e47397f10f
gvbgduh <gvbgduh@gmail.com>
2016-07-05 07:22:01 +0930

    Initial Commit

---
+++ GradingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradingApp
+//
+//  Created by Георгий Богодухов on 5/07/2016.
+//  Copyright © 2016 George Bogodukhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

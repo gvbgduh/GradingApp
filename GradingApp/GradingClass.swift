b788459d3e77cbc883c9e24aaac7472bfa6cd365
gvbgduh <gvbgduh@gmail.com>
2016-07-05 08:13:01 +0930

    Create the ClassesTableView (the first level) and simple classes for data.

---
+++ GradingApp/GradingClass.swift
@@ -0,0 +1,18 @@
+//
+//  GradingClass.swift
+//  GradingApp
+//
+//  Created by Георгий Богодухов on 5/07/2016.
+//  Copyright © 2016 George Bogodukhov. All rights reserved.
+//
+
+import Foundation
+
+class GradingClass {
+    var name = ""
+    var students = []
+    
+    init(name: String) {
+        self.name = name
+    }
+}
---
+++ GradingApp/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  GradingApp
+//
+//  Created by Георгий Богодухов on 5/07/2016.
+//  Copyright © 2016 George Bogodukhov. All rights reserved.
+//
+
+import Foundation
+
+class Student {
+    var firstName = ""
+    var lastName = ""
+    var mark = ""
+    
+    init(firstName:String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
